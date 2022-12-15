1b3dbd59dba671c40057e731e4af9ad129ed1500
SuperSpiritEagle <superspiriteagle@gmail.com>
2022-12-15 17:06:30 +0500

    init

---
+++ IJCycle/IJCycle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IJCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше сообщение");
+            string text = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите количество повторов сообщения");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"{i + 1} {text}");
+            }
+        }
+    }
+}
